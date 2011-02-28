881a0dd2e7866908361c62cfe82f258864379bf2
amarini <>
2011-02-28 22:23:55 +0000

    This commit was manufactured by cvs2svn to create tag 'update_2march2011'.
    Sprout from master 2011-02-28 22:23:54 UTC capalmer '*** empty log message ***'
    Delete:
        .admin/developers
        BuildFile
        development_test/egammatest_35x_cfg.py
        doc/html/index.html
        doc/html/overview.html
        get_all_branches.pl
        h2gglobe/CommonParameters.h
        h2gglobe/GeneralFunctions_cc.h
        h2gglobe/GeneralFunctions_h.h
        h2gglobe/HistoContainer.cc
        h2gglobe/HistoContainer.h
        h2gglobe/LinkDef.h
        h2gglobe/LoopAll.C
        h2gglobe/LoopAll.h
        h2gglobe/LoopAllDict.C
        h2gglobe/LoopAllDict.h
        h2gglobe/LoopAll_cc.h
        h2gglobe/Makefile
        h2gglobe/Makefile.arch
        h2gglobe/PhotonAnalysis/PhotonAnalysisFunctions_cc.h
        h2gglobe/PhotonAnalysis/PhotonAnalysisFunctions_getentry.h
        h2gglobe/PhotonAnalysis/PhotonAnalysisFunctions_h.h
        h2gglobe/PhotonAnalysis/PhotonAnalysisReducedOutputTree.h
        h2gglobe/PhotonAnalysis_scripts/looper.C
        h2gglobe/PhotonAnalysis_scripts/reduce.C
        h2gglobe/Tools.h
        h2gglobe/Util.C
        h2gglobe/Util.h
        h2gglobe/branchdef/Limits.h
        h2gglobe/branchdef/branchdef.h
        h2gglobe/branchdef/getbranch.h
        h2gglobe/branchdef/getentry.h
        h2gglobe/branchdef/newclonesarray.h
        h2gglobe/branchdef/redgetentry.h
        h2gglobe/branchdef/setbranchaddress.h
        h2gglobe/branchdef/treebranch.h
        h2gglobe/branchdef/treedef.h
        h2gglobe/eIDCuts.h
        interface/GlobeAnalyzer.h
        interface/GlobeCaloTowers.h
        interface/GlobeCommon.h
        interface/GlobeCuts.h
        interface/GlobeEcalClusters.h
        interface/GlobeEcalHits.h
        interface/GlobeElectrons.h
        interface/GlobeGenJets.h
        interface/GlobeGenParticles.h
        interface/GlobeGenerator.h
        interface/GlobeGsfTracks.h
        interface/GlobeHLT.h
        interface/GlobeHT.h
        interface/GlobeHcal.h
        interface/GlobeJets.h
        interface/GlobeL1.h
        interface/GlobeLeptons.h
        interface/GlobeMET.h
        interface/GlobeMuons.h
        interface/GlobePFCandidates.h
        interface/GlobePhotons.h
        interface/GlobeReducedGen.h
        interface/GlobeSelector.h
        interface/GlobeSimHits.h
        interface/GlobeSimTracks.h
        interface/GlobeTrackingParticles.h
        interface/GlobeTracks.h
        interface/GlobeVertex.h
        interface/Limits.h
        python/CMSSW_3_3_6_RelValHGG_cfi.py
        python/CMSSW_3_5_4_RelValHGG_cfi.py
        python/CMSSW_3_8_3_RelValHGG_cfi.py
        python/h2ganalyzer_33X_cfi.py
        python/h2ganalyzer_35X_cfi.py
        python/h2ganalyzer_38X_cfi.py
        src/GlobeAnalyzer.cc
        src/GlobeCaloTowers.cc
        src/GlobeCommon.cc
        src/GlobeCuts.cc
        src/GlobeEcalClusters.cc
        src/GlobeEcalHits.cc
        src/GlobeElectrons.cc
        src/GlobeGenJets.cc
        src/GlobeGenParticles.cc
        src/GlobeGenerator.cc
        src/GlobeGsfTracks.cc
        src/GlobeHLT.cc
        src/GlobeHT.cc
        src/GlobeHcal.cc
        src/GlobeJets.cc
        src/GlobeL1.cc
        src/GlobeLeptons.cc
        src/GlobeMET.cc
        src/GlobeMuons.cc
        src/GlobePFCandidates.cc
        src/GlobePhotons.cc
        src/GlobeReducedGen.cc
        src/GlobeSelector.cc
        src/GlobeSimHits.cc
        src/GlobeSimTracks.cc
        src/GlobeTrackingParticles.cc
        src/GlobeTracks.cc
        src/GlobeVertex.cc

--- h2gglobe/PhotonAnalysis/PhotonAnalysisReducedOutputTree.h
+++
@@ -1,5 +0,0 @@
-void LoopAll::PhotonAnalysisReducedOutputTree() {
-  UtilInstance->outputTree->Branch("pho_n", &pho_n, "pho_n/I");
-  UtilInstance->outputTree->Branch("pho_p4", "TClonesArray", &pho_p4, 32000, 0);
-}
-
