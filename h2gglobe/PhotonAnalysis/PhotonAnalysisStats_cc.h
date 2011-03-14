5c88eb8f31c69b0b3821cdc77042b2ef1a0ccef6
nckw <>
2011-03-14 15:26:46 +0000

    DecReview selection added to StatsFunction

--- h2gglobe/PhotonAnalysis/PhotonAnalysisStats_cc.h
+++ h2gglobe/PhotonAnalysis/PhotonAnalysisStats_cc.h
@@ -1,37 +1,66 @@
 
 void LoopAll::myStatPhotonAnalysisRed(Util *ut, int jentry){
 
-   if (PADEBUG)
+  if (PADEBUG)
       cout << "myStatPhotonAnalysRed START" << endl;
 
-   counters[0]++;
+  counters[0]++;
 	
-   b_pho_n->GetEntry(jentry);
-   b_pho_p4->GetEntry(jentry);
-   b_pho_hoe->GetEntry(jentry);
-   b_pho_sieie->GetEntry(jentry);
+  b_pho_n->GetEntry(jentry); 
+  b_pho_p4->GetEntry(jentry); 
+  b_pho_hoe->GetEntry(jentry); 
+  b_pho_sieie->GetEntry(jentry); 
+  b_pho_ecalsumetconedr03->GetEntry(jentry); 
+  b_pho_ecalsumetconedr04->GetEntry(jentry); 
+  b_pho_hcalsumetconedr03->GetEntry(jentry); 
+  b_pho_hcalsumetconedr04->GetEntry(jentry); 
+  b_pho_trksumpthollowconedr04->GetEntry(jentry); 
+  b_pho_haspixseed->GetEntry(jentry); 
 
-   Float_t best_mass = 0;
-   for (int i=0; i<pho_n-1; i++) {
-     TLorentzVector *pg1= (TLorentzVector *) pho_p4->At(i);
+  std::vector<TLorentzVector *> selected_photons;  
+
+  for (int i=0; i<pho_n; i++) {
+    TLorentzVector *p4 = (TLorentzVector *) pho_p4->At(i);
+    float pt  = p4->Pt();
+    float eta = fabs(p4->Eta());
+    if(
+	(! (pho_haspixseed[i]==1))
+     &&	(pt > 30. )
+     && (pho_hoe[i] < 0.02)
+     && (pho_trksumpthollowconedr04[i] < (1.5 + 0.001*pt))
+     && (pho_ecalsumetconedr04[i] < (2.0 + 0.006*pt)	)
+     && (pho_hcalsumetconedr04[i] < (2.0 + 0.0025*pt)	)
+     && (   ((pho_sieie[i] < 0.01) && (eta < 1.44)) 
+	 || ((pho_sieie[i] <0.028) && ((eta < 2.5) && (eta > 1.57))) 
+	)
+     && ((eta < 1.4442) || ((eta > 1.566) && (eta < 2.5)) )
  
-     for (int j=i+1; j<pho_n; j++) {
-       TLorentzVector *pg2= (TLorentzVector *) pho_p4->At(j);
-       TLorentzVector higgs = (*pg1) + (*pg2);
-       Float_t mass = higgs.M();
-       if (mass > best_mass){
-        if ( pho_hoe[i]          < 0.05   && pho_hoe[j]         < 0.05
-         && pho_sieie[i]         < 0.03    && pho_sieie[j]       < 0.03
-         && pg1->Pt()            > 30      && pg2->Pt()          > 30
-         && fabs(pg1->Eta())     < 2.4     && fabs(pg2->Eta())   < 2.4
-         )
-         best_mass = mass;
-        }
-       }
-   }
-   if (best_mass != 0){
+	){
+		selected_photons.push_back(p4);
+     }
+  }
+
+  int n_selected_pho = selected_photons.size();
+
+  // now make sure there is one photon with Pt > 40 in the candidates
+  float best_mass = 0.;
+  
+  if (n_selected_pho > 1) {
+	
+	for (int i=0; i< n_selected_pho-1; i++)
+	  for (int j=i+1;j<n_selected_pho;j++){
+		
+		if (selected_photons[i]->Pt() > 40. 
+		 || selected_photons[j]->Pt() > 40.){
+				
+		  TLorentzVector Higgs = (*selected_photons[i])+(*selected_photons[j]);
+		  float mass = Higgs.M();
+		  if (mass > best_mass) best_mass = mass;
+		}
+	  }	
+  }
      rooContainer->SetRealVar("mass",best_mass);
-   }
+  
  
    if(PADEBUG)
      cout<<"myStatPhotonAnalysisRed END"<<endl;
