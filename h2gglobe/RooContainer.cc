9372f51693352ecef0ecdb5b2ab0cc5b71ed48b0
nckw <>
2011-03-24 16:05:19 +0000

    Added extended Pdf

--- h2gglobe/RooContainer.cc
+++ h2gglobe/RooContainer.cc
@@ -1,11 +1,13 @@
 #include "RooContainer.h"
-#include <RooArgSet.h>
-#include <RooArgList.h>
-#include <RooFitResult.h>
-
+#include "RooArgSet.h"
+#include "RooArgList.h"
+#include "RooFitResult.h"
+#include "RooAddPdf.h"
+#include "RooGlobalFunc.h"
 #include <cmath>
 
 using namespace RooFit;
+
 
 void RooContainer::AddRealVar(const char* name ,float xmin,float xmax){
   RooRealVar temp(name,name,xmin,xmax);
@@ -14,17 +16,18 @@ void RooContainer::AddRealVar(const char* name ,float xmin,float xmax){
   m_var_min_.insert(pair<const char*, float>(name,xmin));
   m_var_max_.insert(pair<const char*, float>(name,xmax));
   
-  std::cout << "Added Variale"  << name << std::endl;
-  
 }
 
 void RooContainer::AddRealVar(const char* name ,float init, float vmin, float vmax){
   RooRealVar temp(name,name,init,vmin,vmax);
   m_real_var_.insert(pair<const char*,RooRealVar>(name,temp));
+  std::cout << "Appended the variable " << name << "To the RealVars" <<std::endl;
   
 }
 
-void RooContainer::AddGenericPdf(const char* name,const char* formula, std::vector<const char*> & var){
+void RooContainer::AddGenericPdf(const char* name,const char* formula
+				,std::vector<const char*> & var
+				,double norm ){
 
     RooArgList roo_args;
 
@@ -36,10 +39,43 @@ void RooContainer::AddGenericPdf(const char* name,const char* formula, std::vect
 	  roo_args.add(m_real_var_[*it_var]);
 	}
 
-    RooGenericPdf temp(name,name,formula,roo_args);
-  			       
-    m_exp_.insert(pair<const char*,RooGenericPdf>(name,temp));
+
+
+    std::cout << "Added all variables" << std::endl;
+    RooGenericPdf temp_1(Form("comp_%s",name),name,formula,roo_args);	
+    m_gen_.insert(std::pair<const char *, RooGenericPdf>(name,temp_1));
+
+    RooRealVar temp_var(Form("norm_%s",name),name,norm,0.0,10000);
+    m_real_var_.insert(pair<const char*,RooRealVar>(name,temp_var));
+    std::cout << "still sweet " <<std::endl;
+
+    RooExtendPdf  temp(name,name,m_gen_[name],m_real_var_[name]);
+
+    std::cout << "Made extended PDF" << name << std::endl;			       
+    m_exp_.insert(pair<const char*,RooExtendPdf>(name,temp));
 }
+
+void RooContainer::ComposePdf(const char* name, const char * composition
+			     ,std::vector<const char*> & formula){
+
+    RooArgList roo_args;
+    RooArgList roo_funs;
+
+    for (std::vector<const char*>::iterator it_fun = formula.begin()
+	;it_fun != formula.end()
+	;it_fun++
+	){
+	  std::cout << "Including Function " << *it_fun << std::endl;
+	  roo_funs.add(m_exp_[*it_fun]);
+	  roo_args.add(m_real_var_[(*it_fun)]);
+	}
+
+    RooAddPdf temp(name,composition,roo_funs,roo_args);
+    //RooAddPdf temp(name,composition,roo_funs);
+    std::cout << "Created Composed PDF" << name << std::endl;			       
+    m_pdf_.insert(pair<const char*,RooAddPdf>(name,temp));
+}
+
 
 void RooContainer::CreateDataSet(const char *name){
  
@@ -48,15 +84,95 @@ void RooContainer::CreateDataSet(const char *name){
 }
 
 void RooContainer::FitToData(const char* name_func, const char * name_var){
- 
-    RooFitResult *fit_result = m_exp_[name_func].fitTo(*data_);
+
+    bool use_composed_pdf = false;
+    std::cout << "Fitting function " << name_func << " To data" <<std::endl; 
+
+    // Look in the composed pdf before checking the standards
+    std::map<const char *,RooAddPdf>::const_iterator it_pdf_ = m_pdf_.find(name_func);
+    if (it_pdf_ != m_pdf_.end()){
+      RooFitResult *fit_result = m_pdf_[name_func].fitTo(*data_);
+      use_composed_pdf = true;
+    }
+    else {
+      RooFitResult *fit_result = m_exp_[name_func].fitTo(*data_);
+    }
 
     float x_min = m_var_min_[name_var];
     float x_max = m_var_max_[name_var];
  
     RooPlot *xframe = m_real_var_[name_var].frame(x_min,x_max);
     data_->plotOn(xframe);
-    m_exp_[name_func].plotOn(xframe);
+
+    if (use_composed_pdf){
+      m_pdf_[name_func].plotOn(xframe,LineColor(4));
+      int npdfs = m_pdf_[name_func].pdfList().getSize();
+    
+      for (int i=0;i<npdfs;i++){
+	m_pdf_[name_func].plotOn(xframe
+				,Components(*(m_pdf_[name_func].pdfList().at(i)))
+				,LineColor(i+1)
+				,LineStyle(kDashed));
+      } 
+    }
+    else m_exp_[name_func].plotOn(xframe,LineColor(4));
+ 
+    xframe->Draw();
+}
+
+void RooContainer::FitToData(const char* name_func, const char * name_var
+			    ,double x1, double x2, double x3, double x4){
+
+    
+    float x_min = m_var_min_[name_var];
+    float x_max = m_var_max_[name_var];
+
+    bool use_composed_pdf = false;
+    std::cout << "Fitting function " << name_func << " To data" <<std::endl; 
+
+    // Look in the composed pdf before checking the standards
+    std::map<const char *,RooAddPdf>::const_iterator it_pdf_ = m_pdf_.find(name_func);
+    if (it_pdf_ != m_pdf_.end()){
+
+      if (x1 < x_min || x4 > x_max){
+        std::cout << "Ranges outside of DataSet Range!" << std::endl;
+        RooFitResult *fit_result = m_pdf_[name_func].fitTo(*data_);
+        std::cout << "Fitted To Full Range" << std::endl;
+      } else {
+        m_real_var_[name_var].setRange("rnge1",x1,x2);
+        m_real_var_[name_var].setRange("rnge2",x3,x4);
+        RooFitResult *fit_result = m_pdf_[name_func].fitTo(*data_,Range("rnge1,rnge2"));
+      }
+      use_composed_pdf = true;
+    }
+    else {
+     if (x1 < x_min || x4 > x_max){
+        std::cout << "Ranges outside of DataSet Range!" << std::endl;
+        RooFitResult *fit_result = m_exp_[name_func].fitTo(*data_);
+        std::cout << "Fitted To Full Range" << std::endl;
+      } else {
+        m_real_var_[name_var].setRange("rnge1",x1,x2);
+        m_real_var_[name_var].setRange("rnge2",x3,x4);
+        RooFitResult *fit_result = m_exp_[name_func].fitTo(*data_,Range("rnge1,rnge2"));
+      }
+    }
+
+    RooPlot *xframe = m_real_var_[name_var].frame(x_min,x_max);
+    data_->plotOn(xframe);
+
+    if (use_composed_pdf){
+      m_pdf_[name_func].plotOn(xframe,LineColor(4));
+      int npdfs = m_pdf_[name_func].pdfList().getSize();
+    
+      for (int i=0;i<npdfs;i++){
+	m_pdf_[name_func].plotOn(xframe
+			,Components(*(m_pdf_[name_func].pdfList().at(i)))
+			,LineColor(i+1)
+			,LineStyle(kDashed));
+      }
+    }
+
+    else m_exp_[name_func].plotOn(xframe,LineColor(4));
  
     xframe->Draw();
 }
--- h2gglobe/RooContainer.h
+++ h2gglobe/RooContainer.h
@@ -2,11 +2,15 @@
 #define ROOCONTAINER
 
 // RooFit includes
-#include <RooDataSet.h>
-#include <RooRealVar.h>
-#include <RooPlot.h>
-#include <RooExponential.h>
-#include <RooGenericPdf.h>
+#include "RooDataSet.h"
+#include "RooRealVar.h"
+#include "RooPlot.h"
+#include "RooExponential.h"
+#include "RooGenericPdf.h"
+#include "RooExtendPdf.h"
+#include "RooAddPdf.h"
+
+#include "TRandom3.h"
 
 #include <map>
 #include <vector>
@@ -16,28 +20,37 @@
 class RooContainer {
 
   public:
-   RooContainer(){};
+   RooContainer(){}
    ~RooContainer(){};
+
+   double rand_n();
    
    void AddRealVar(const char*,float,float);
    void AddRealVar(const char*,float,float,float);
    void AddGenericPdf(const char*,const char*,
-		      std::vector<const char*> &);   
+		      std::vector<const char*> &, double norm=10);
+   void ComposePdf(const char* , const char *
+			     ,std::vector<const char*> &);
 
    void CreateDataSet(const char*);
    void SetRealVar(const char * name, float x);
    
    void FitToData(const char*,const char*);
+   void FitToData(const char*,const char *
+	         ,double, double, double, double);
 
    
   private:
    std::map<const char*, RooRealVar> m_real_var_;
-   std::map<const char*, RooGenericPdf> m_exp_;
+   std::map<const char*, RooExtendPdf> m_exp_;
+   std::map<const char*, RooGenericPdf> m_gen_;
+   std::map<const char*, RooAddPdf> m_pdf_;
 
    std::map<const char*, float> m_var_min_;
    std::map<const char*, float> m_var_max_;
 
    RooDataSet *data_;
+   
 
 };
 
