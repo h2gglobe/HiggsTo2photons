daf3bfd13f5228a8c3829aceb10498a42d42899d
drberry <>
2011-10-07 08:36:32 +0000

    Added Single Leg Conversions

--- src/GlobeConversions.cc
+++ src/GlobeConversions.cc
@@ -15,10 +15,17 @@ GlobeConversions::GlobeConversions(const edm::ParameterSet& iConfig, const char*
   hybridSuperClusterColl = iConfig.getParameter<edm::InputTag>("HybridSuperClusterColl");
   endcapSuperClusterColl = iConfig.getParameter<edm::InputTag>("EndcapSuperClusterColl");
 
+  // PHOTONS 
+  photonCollStd =  iConfig.getParameter<edm::InputTag>("PhotonCollStd");
+  
+  // Particle Flow
+  pfColl = iConfig.getParameter<edm::InputTag>("PFCandidateColl");
+  
   // get cut thresholds
   gCUT = new GlobeCuts(iConfig);
 
   conv_nHitsBeforeVtx = new std::vector<std::vector<unsigned short> >; conv_nHitsBeforeVtx->clear();
+  conv_quality = new std::vector<std::vector<int> >; conv_quality->clear();
 
 }
 
@@ -36,7 +43,8 @@ void GlobeConversions::defineBranch(TTree* tree) {
   tree->Branch("conv_dphitrksatvtx",&conv_dphitrksatvtx,"conv_dphitrksatvtx[conv_n]/F");
   tree->Branch("conv_dphitrksatecal",&conv_dphitrksatecal,"conv_dphitrksatecal[conv_n]/F");
   tree->Branch("conv_detatrksatecal",&conv_detatrksatecal,"conv_detatrksatecal[conv_n]/F");
-  tree->Branch("conv_quality",&conv_quality,"conv_quality[conv_n]/I");
+  tree->Branch("conv_quality","std::vector<std::vector<int> >",&conv_quality);
+  tree->Branch("conv_type",&conv_type,"conv_type[conv_n]/I");
   tree->Branch("conv_dxy",&conv_dxy,"conv_dxy[conv_n]/F"); // will not be filled because this will only be available from 420
   tree->Branch("conv_dz",&conv_dz,"conv_dz[conv_n]/F");    // will not be filled because this will only be available from 420
   tree->Branch("conv_lxy",&conv_lxy,"conv_lxy[conv_n]/F"); // will not be filled because this will only be available from 420
@@ -46,7 +54,7 @@ void GlobeConversions::defineBranch(TTree* tree) {
   tree->Branch("conv_nSharedHits",&conv_nSharedHits,"conv_nSharedHits[conv_n]/I");
 
   tree->Branch("conv_validvtx",&conv_validvtx,"conv_validvtx[conv_n]/I");
-  tree->Branch("conv_MVALikelihood",&conv_MVALikelihood,"conv_MVALikelihood[conv_n]/I");
+  tree->Branch("conv_MVALikelihood",&conv_MVALikelihood,"conv_MVALikelihood[conv_n]/F");
   /// vertex quantities 
   tree->Branch("conv_chi2",&conv_chi2,"conv_chi2[conv_n]/F");
   tree->Branch("conv_chi2_probability",&conv_chi2_probability,"conv_chi2_probability[conv_n]/F");
@@ -59,8 +67,8 @@ void GlobeConversions::defineBranch(TTree* tree) {
   tree->Branch("conv_tk2_dz",&conv_tk2_dz,"conv_tk2_dz[conv_n]/F");
   tree->Branch("conv_tk1_dzerr",&conv_tk1_dzerr,"conv_tk1_dzerr[conv_n]/F");
   tree->Branch("conv_tk2_dzerr",&conv_tk2_dzerr,"conv_tk2_dzerr[conv_n]/F");
-  tree->Branch("conv_tk1_nh",&conv_tk1_nh,"conv_tk1_nh[conv_n]/I");
-  tree->Branch("conv_tk2_nh",&conv_tk2_nh,"conv_tk2_nh[conv_n]/I");
+  tree->Branch("conv_tk1_nh",&conv_tk1_nh,"conv_tk1_nh[conv_n]/S");
+  tree->Branch("conv_tk2_nh",&conv_tk2_nh,"conv_tk2_nh[conv_n]/S");
   tree->Branch("conv_ch1ch2",&conv_ch1ch2,"conv_ch1ch2[conv_n]/I");
   tree->Branch("conv_tk1_d0",&conv_tk1_d0,"conv_tk1_d0[conv_n]/F");
   tree->Branch("conv_tk1_pout",&conv_tk1_pout,"conv_tk1_pout[conv_n]/F");
@@ -69,16 +77,14 @@ void GlobeConversions::defineBranch(TTree* tree) {
   tree->Branch("conv_tk2_pout",&conv_tk2_pout,"conv_tk2_pout[conv_n]/F");
   tree->Branch("conv_tk2_pin",&conv_tk2_pin,"conv_tk2_pin[conv_n]/F");
 
-
-
-
-
   conv_vtx = new TClonesArray("TVector3", MAX_CONVERTEDPHOTONS);
   tree->Branch("conv_vtx", "TClonesArray", &conv_vtx, 32000, 0);
   conv_pair_momentum = new TClonesArray("TVector3", MAX_CONVERTEDPHOTONS);
   tree->Branch("conv_pair_momentum", "TClonesArray", &conv_pair_momentum, 32000, 0);
   conv_refitted_momentum = new TClonesArray("TVector3", MAX_CONVERTEDPHOTONS);
   tree->Branch("conv_refitted_momentum", "TClonesArray", &conv_refitted_momentum, 32000, 0);
+  conv_singleleg_momentum = new TClonesArray("TVector3", MAX_CONVERTEDPHOTONS);
+  tree->Branch("conv_singleleg_momentum", "TClonesArray", &conv_singleleg_momentum, 32000, 0);
 
 }
 
@@ -92,6 +98,12 @@ bool GlobeConversions::analyze(const edm::Event& iEvent, const edm::EventSetup& 
   edm::Handle<reco::ConversionCollection> convH;
   iEvent.getByLabel(allConversionsColl, convH);
 
+  edm::Handle<reco::PhotonCollection> PhoH;
+  iEvent.getByLabel(photonCollStd, PhoH);
+  
+  edm::Handle<reco::PFCandidateCollection> pfHandle;
+  iEvent.getByLabel(pfColl, pfHandle);
+  
   if (debug_level > 9) {
     std::cout << "GlobeConversions: Start analyze" << std::endl;
   }
@@ -117,7 +129,9 @@ bool GlobeConversions::analyze(const edm::Event& iEvent, const edm::EventSetup& 
   conv_vtx->Clear();
   conv_pair_momentum->Clear();
   conv_refitted_momentum->Clear();
+  conv_singleleg_momentum->Clear();
   conv_nHitsBeforeVtx->clear();
+  conv_quality->clear();
    
   conv_n = 0;
 
@@ -136,6 +150,7 @@ bool GlobeConversions::analyze(const edm::Event& iEvent, const edm::EventSetup& 
     new ((*conv_vtx)[conv_n]) TVector3();
     new ((*conv_pair_momentum)[conv_n]) TVector3();
     new ((*conv_refitted_momentum)[conv_n]) TVector3();
+    new ((*conv_singleleg_momentum)[conv_n]) TVector3();
 
     if(debug_level>9)std::cout << "GlobeConversions: -21 "<< std::endl;
 
@@ -197,7 +212,7 @@ bool GlobeConversions::analyze(const edm::Event& iEvent, const edm::EventSetup& 
     conv_dphitrksatvtx[conv_n]=-999.;
     conv_dphitrksatecal[conv_n]=-999.;
     conv_detatrksatecal[conv_n]=-999.;
-    conv_quality[conv_n]=-999.;
+    conv_type[conv_n]=-999.;
     conv_dxy[conv_n]=-999.;
     conv_dz[conv_n]=-999.;
     conv_lxy[conv_n]=-999.;
@@ -215,8 +230,10 @@ bool GlobeConversions::analyze(const edm::Event& iEvent, const edm::EventSetup& 
     conv_ch1ch2[conv_n] = -999.;
     conv_tk1_dz[conv_n]=-999.;
     conv_tk1_dzerr[conv_n]=-999.;
+    conv_tk1_nh[conv_n]=-999.;
     conv_tk2_dz[conv_n]=-999.;
     conv_tk2_dzerr[conv_n]=-999.;
+    conv_tk2_nh[conv_n]=-999.;
     conv_vtx_xErr[conv_n]=-999.;
     conv_vtx_yErr[conv_n]=-999.;
     conv_vtx_zErr[conv_n]=-999.;
@@ -224,6 +241,7 @@ bool GlobeConversions::analyze(const edm::Event& iEvent, const edm::EventSetup& 
     ((TVector3 *)conv_vtx->At(conv_n))->SetXYZ(-999, -999, -999);
     ((TVector3 *)conv_pair_momentum->At(conv_n))->SetXYZ(-999, -999, -999);
     ((TVector3 *)conv_refitted_momentum->At(conv_n))->SetXYZ(-999, -999, -999);
+    ((TVector3 *)conv_singleleg_momentum->At(conv_n))->SetXYZ(-999, -999, -999);
 
     if (debug_level>9) std::cout << "Looking For Valid Conversion" << std::endl;
     if (debug_level>9) std::cout << "Checking Vertex Validity" << std::endl;
@@ -253,11 +271,13 @@ bool GlobeConversions::analyze(const edm::Event& iEvent, const edm::EventSetup& 
         if(i==0) {
           conv_tk1_dz[conv_n]=tracks[i]->dz();
           conv_tk1_dzerr[conv_n]=tracks[i]->dzError();
+          conv_tk1_nh[conv_n]=tracks[i]->numberOfValidHits();
           conv_ch1ch2[conv_n]=tracks[i]->charge();
         }
         else if(i==1) {
           conv_tk2_dz[conv_n]=tracks[i]->dz();
           conv_tk2_dzerr[conv_n]=tracks[i]->dzError();
+          conv_tk2_nh[conv_n]=tracks[i]->numberOfValidHits();
           conv_ch1ch2[conv_n]*=tracks[i]->charge();
         }
       }
@@ -278,11 +298,7 @@ bool GlobeConversions::analyze(const edm::Event& iEvent, const edm::EventSetup& 
     conv_lxy[conv_n]=localConv.lxy();
     conv_lz[conv_n]=localConv.lz();
 
-    int flag=-999;
-    if (localConv.quality(reco::Conversion::arbitratedMerged)) flag= 1;
-    if (localConv.quality(reco::Conversion::generalTracksOnly)) flag =2;
-    if (localConv.quality(reco::Conversion::arbitratedEcalSeeded)) flag=3;
-    conv_quality[conv_n]=flag;
+    conv_type[conv_n]=2;
     
     std::vector<unsigned short> tmp;
     for (unsigned int i=0; i<localConv.nHitsBeforeVtx().size(); ++i) {
@@ -292,6 +308,13 @@ bool GlobeConversions::analyze(const edm::Event& iEvent, const edm::EventSetup& 
     conv_nHitsBeforeVtx->push_back(tmp);
     conv_nSharedHits[conv_n] = localConv.nSharedHits();
 
+    std::vector<int> conv_quality_tmp;
+    if (localConv.quality(reco::Conversion::arbitratedMerged)) conv_quality_tmp.push_back(1);
+    if (localConv.quality(reco::Conversion::generalTracksOnly)) conv_quality_tmp.push_back(2);
+    if (localConv.quality(reco::Conversion::arbitratedEcalSeeded)) conv_quality_tmp.push_back(3);
+    conv_quality->push_back(conv_quality_tmp);
+    //std::cout << "Conversion Quality Size: " << conv_quality->size() << "Conversion Quality Temp Size: " << conv_quality_tmp.size() << " Number of Conversions: " << conv_n << std::endl;
+    
     if(localConv.tracks().size() > 0) {
       conv_tk1_d0[conv_n]=localConv.tracksSigned_d0()[0];
       conv_tk1_pout[conv_n]=sqrt(localConv.tracksPout()[0].Mag2());
@@ -307,7 +330,185 @@ bool GlobeConversions::analyze(const edm::Event& iEvent, const edm::EventSetup& 
     conv_n++;
   }
 
+  // Get Particle Flow Conversions
+  for (reco::PhotonCollection::const_iterator localPho=PhoH->begin(); localPho!=PhoH->end(); localPho++) {
+    for (reco::PFCandidateCollection::const_iterator iPfCand=pfHandle->begin(); iPfCand!=pfHandle->end(); iPfCand++) {
+      if (iPfCand->particleId()!=reco::PFCandidate::gamma) continue;
+      if (iPfCand->mva_nothing_gamma()<=0) continue;
+      reco::PhotonRef phoRef = iPfCand->photonRef();
+      if (localPho->superCluster()!=phoRef->superCluster()) continue;
+      reco::ConversionRefVector convsingleleg = phoRef->conversionsOneLeg();
+      for (unsigned int iconvoneleg=0; iconvoneleg<convsingleleg.size(); iconvoneleg++){
 
+        if (conv_n >= MAX_CONVERTEDPHOTONS) {
+          std::cout << "GlobeConversions: WARNING TOO MANY CONVERSIONS: " << convH->size()+convsingleleg.size() << " (allowed " << MAX_CONVERTEDPHOTONS << ")" << std::endl;
+          break;
+        }
+        reco::Conversion localConv = reco::Conversion(*convsingleleg[iconvoneleg]);
+        if(gCUT->cut(localConv)) continue;
+
+        new ((*conv_p4)[conv_n]) TLorentzVector();
+        new ((*conv_vtx)[conv_n]) TVector3();
+        new ((*conv_pair_momentum)[conv_n]) TVector3();
+        new ((*conv_refitted_momentum)[conv_n]) TVector3();
+        new ((*conv_singleleg_momentum)[conv_n]) TVector3();
+
+        ((TLorentzVector *)conv_p4->At(conv_n))->SetXYZT(localConv.refittedPair4Momentum().px(), localConv.refittedPair4Momentum().py(), localConv.refittedPair4Momentum().pz(), localConv.refittedPair4Momentum().energy());
+
+        conv_scind[conv_n] = -1;
+
+        int index = 0;
+        if ( localConv.caloCluster().size() ) {
+
+          for(int isuperClusterType=0; isuperClusterType<3; ++isuperClusterType) {
+            if (isuperClusterType == 0) {
+              for(reco::SuperClusterCollection::size_type j = 0; j<superClustersHybridH->size(); ++j){
+	    
+                reco::SuperClusterRef sc(superClustersHybridH, j);
+	    
+                //apply cuts
+                if(gCUT->cut(*sc))continue;
+                //passed cuts
+                if (  sc.id() == localConv.caloCluster()[0].id() && sc.key() == localConv.caloCluster()[0].key() ) {
+                  conv_scind[conv_n] = index;
+                  break;
+                }
+                index++;
+              }
+            }
+	
+            if (isuperClusterType == 2) {
+              for(reco::SuperClusterCollection::size_type j = 0; j<superClustersEndcapH->size(); ++j){
+	    
+                reco::SuperClusterRef sc(superClustersEndcapH, j);
+                //apply cuts
+                if(gCUT->cut(*sc))continue;
+                //passed cuts
+	    
+                if ( sc.id() == localConv.caloCluster()[0].id() && sc.key() == localConv.caloCluster()[0].key() ) {
+                  conv_scind[conv_n] = index;
+                  break;
+                }
+                index++;
+              }
+            }
+          }
+        }
+
+        conv_ntracks[conv_n]=0;
+        conv_pairinvmass[conv_n]=-999.;
+        conv_paircotthetasep[conv_n]=-999.;
+        conv_eoverp[conv_n]=-999.;
+        conv_distofminapproach[conv_n]=-999.;
+        conv_dphitrksatvtx[conv_n]=-999.;
+        conv_dphitrksatecal[conv_n]=-999.;
+        conv_detatrksatecal[conv_n]=-999.;
+        conv_type[conv_n]=-999.;
+        conv_dxy[conv_n]=-999.;
+        conv_dz[conv_n]=-999.;
+        conv_lxy[conv_n]=-999.;
+        conv_lz[conv_n]=-999.;
+        conv_nSharedHits[conv_n]=0;
+        conv_zofprimvtxfromtrks[conv_n]=-999.;
+        conv_tk1_d0[conv_n]=-999.;
+        conv_tk1_pout[conv_n]=-999.;
+        conv_tk1_pin[conv_n]=-999.;
+        conv_tk2_d0[conv_n]=-999.;
+        conv_tk2_pout[conv_n]=-999.;
+        conv_tk2_pin[conv_n]=-999.;
+        conv_validvtx[conv_n]=0;
+        conv_MVALikelihood[conv_n]=-999.;
+        conv_ch1ch2[conv_n] = -999.;
+        conv_tk1_dz[conv_n]=-999.;
+        conv_tk1_dzerr[conv_n]=-999.;
+        conv_tk1_nh[conv_n]=-999.;
+        conv_tk2_dz[conv_n]=-999.;
+        conv_tk2_dzerr[conv_n]=-999.;
+        conv_tk2_nh[conv_n]=-999.;
+        conv_vtx_xErr[conv_n]=-999.;
+        conv_vtx_yErr[conv_n]=-999.;
+        conv_vtx_zErr[conv_n]=-999.;
+
+        ((TVector3 *)conv_vtx->At(conv_n))->SetXYZ(-999, -999, -999);
+        ((TVector3 *)conv_pair_momentum->At(conv_n))->SetXYZ(-999, -999, -999);
+        ((TVector3 *)conv_refitted_momentum->At(conv_n))->SetXYZ(-999, -999, -999);
+        ((TVector3 *)conv_singleleg_momentum->At(conv_n))->SetXYZ(-999, -999, -999);
+
+        conv_validvtx[conv_n]=localConv.conversionVertex().isValid();
+        if ( !localConv.conversionVertex().isValid() ) continue;
+        reco::Vertex vtx=localConv.conversionVertex();
+    
+    
+        ((TVector3 *) conv_vtx->At(conv_n))->SetXYZ(vtx.x(), vtx.y(), vtx.z());
+        ((TVector3 *) conv_pair_momentum->At(conv_n))->SetXYZ(localConv.pairMomentum().x(), localConv.pairMomentum().y(), localConv.pairMomentum().z());
+        ((TVector3 *) conv_refitted_momentum->At(conv_n))->SetXYZ(localConv.refittedPairMomentum().x(), localConv.refittedPairMomentum().y(), localConv.refittedPairMomentum().z());
+
+
+        conv_chi2[conv_n]=vtx.chi2();
+        conv_chi2_probability[conv_n]=ChiSquaredProbability(vtx.chi2(), vtx.ndof());
+        conv_vtx_xErr[conv_n]= vtx.xError();
+        conv_vtx_yErr[conv_n]= vtx.yError();
+        conv_vtx_zErr[conv_n]= vtx.zError();
+        conv_ntracks[conv_n]=localConv.nTracks();
+        if (localConv.oneLegMVA().size()>1) std::cout << "Warning: More then one MVA value for single leg conversion MVA." << std::endl;
+        conv_MVALikelihood[conv_n]=localConv.oneLegMVA()[0];
+
+
+        if( localConv.nTracks()) {
+          const std::vector<edm::RefToBase<reco::Track> > tracks = localConv.tracks();
+          if (tracks.size()>0) {
+            conv_tk1_dz[conv_n]=tracks[0]->dz();
+            conv_tk1_dzerr[conv_n]=tracks[0]->dzError();
+            conv_tk1_nh[conv_n]=tracks[0]->numberOfValidHits();
+            conv_ch1ch2[conv_n]=tracks[0]->charge();
+            ((TVector3 *) conv_singleleg_momentum->At(conv_n))->SetXYZ(tracks[0]->innerMomentum().X(), tracks[0]->innerMomentum().Y(), tracks[0]->innerMomentum().Z());
+          }
+        }
+
+    
+        conv_pairinvmass[conv_n]=localConv.pairInvariantMass();
+        conv_paircotthetasep[conv_n]=localConv.pairCotThetaSeparation();
+        // will work in 420 conv_eoverp[conv_n]=localConv.EoverPrefittedTracks();
+        conv_zofprimvtxfromtrks[conv_n]=localConv.zOfPrimaryVertexFromTracks();
+        conv_distofminapproach[conv_n]=localConv.distOfMinimumApproach();
+        conv_dphitrksatvtx[conv_n]=localConv.dPhiTracksAtVtx();
+        //conv_dphitrksatecal[conv_n]=localConv.dPhiTracksAtEcal();
+        //conv_detatrksatecal[conv_n]=localConv.dEtaTracksAtEcal();
+        //commented out for now since these will only be available in cmssw_420
+        conv_dxy[conv_n]=localConv.dxy();
+        conv_dz[conv_n]=localConv.dz();
+        conv_lxy[conv_n]=localConv.lxy();
+        conv_lz[conv_n]=localConv.lz();
+
+        conv_type[conv_n]=1;
+    
+        std::vector<unsigned short> tmp;
+        for (unsigned int i=0; i<localConv.nHitsBeforeVtx().size(); ++i) {
+          tmp.push_back(static_cast<unsigned short>(localConv.nHitsBeforeVtx()[i]));
+        }
+
+        conv_nHitsBeforeVtx->push_back(tmp);
+        conv_nSharedHits[conv_n] = localConv.nSharedHits();
+
+        std::vector<int> conv_quality_tmp;
+        if (localConv.quality(reco::Conversion::arbitratedMerged)) conv_quality_tmp.push_back(1);
+        if (localConv.quality(reco::Conversion::generalTracksOnly)) conv_quality_tmp.push_back(2);
+        if (localConv.quality(reco::Conversion::arbitratedEcalSeeded)) conv_quality_tmp.push_back(3);
+        conv_quality->push_back(conv_quality_tmp);
+        //std::cout << "Conversion Quality Size: " << conv_quality->size() << "Conversion Quality Temp Size: " << conv_quality_tmp.size() << " Number of Conversions: " << conv_n << std::endl;
+
+        if(localConv.tracks().size() > 0) {
+          conv_tk1_d0[conv_n]=localConv.tracksSigned_d0()[0];
+          conv_tk1_pout[conv_n]=sqrt(localConv.tracksPout()[0].Mag2());
+          conv_tk1_pin[conv_n]=sqrt(localConv.tracksPin()[0].Mag2());
+        }
+
+        conv_n++;
+        
+      }
+    }
+  }
+  
   if(debug_level>9)
     std::cout << "End Conversion" << std::endl;
 
