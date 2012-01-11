6187994c8cf1ebfebb2e7d21c78d026cb81c9471
futyand <>
2012-01-11 20:37:30 +0000

    remove bias calculation and plots (only slopes needed)

--- h2gglobe/Macros/runMassBias.sh
+++ h2gglobe/Macros/runMassBias.sh
@@ -2,8 +2,7 @@
 
 rm BkgBias.root
 
-root -b -l -q 'massBias.C('115',true)'
-setenv M 120
+setenv M 115
 while ( $M <= 150 )
   if ( $M != 145 ) root -b -l -q 'massBias.C('${M}')'
   setenv M `expr $M + 5`
