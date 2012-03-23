f56def778cd18c7a2b1f57967fc15a1a050d0eb0
capalmer <>
2012-03-23 14:03:18 +0000

    adding solid hcal isolation

--- interface/GlobeElectrons.h
+++ interface/GlobeElectrons.h
@@ -107,9 +107,11 @@ class GlobeElectrons {
   Float_t el_tkiso03[MAX_ELECTRONS];
   Float_t el_ecaliso03[MAX_ELECTRONS];
   Float_t el_hcaliso03[MAX_ELECTRONS];
+  Float_t el_hcalsolidiso03[MAX_ELECTRONS];
   Float_t el_tkiso04[MAX_ELECTRONS];
   Float_t el_ecaliso04[MAX_ELECTRONS];
   Float_t el_hcaliso04[MAX_ELECTRONS];
+  Float_t el_hcalsolidiso04[MAX_ELECTRONS];
 
   Float_t el_mva[MAX_ELECTRONS];
   Bool_t el_ecaldrv[MAX_ELECTRONS];
@@ -162,6 +164,7 @@ class GlobeElectrons {
   // SUPER CLUSTERS
   edm::InputTag hybridSuperClusterColl;
   edm::InputTag endcapSuperClusterColl;  
+  edm::InputTag caloTowerColl;
   edm::InputTag ecalHitEBColl;
   edm::InputTag ecalHitEEColl;
   edm::InputTag ecalHitESColl;
--- src/GlobeElectrons.cc
+++ src/GlobeElectrons.cc
@@ -7,6 +7,10 @@
 #include "DataFormats/GsfTrackReco/interface/GsfTrack.h"
 #include "RecoEcal/EgammaCoreTools/interface/EcalClusterTools.h"
 #include "RecoEgamma/EgammaTools/interface/ConversionTools.h"
+#include "DataFormats/CaloTowers/interface/CaloTower.h"
+#include "DataFormats/CaloTowers/interface/CaloTowerCollection.h"
+
+#include "RecoEgamma/EgammaIsolationAlgos/interface/EgammaTowerIsolation.h"
 
 #include "HiggsAnalysis/HiggsTo2photons/interface/PFIsolation.h"
 
@@ -22,6 +26,7 @@ GlobeElectrons::GlobeElectrons(const edm::ParameterSet& iConfig, const char* n):
 
   conversionColl = iConfig.getParameter<edm::InputTag>("ConvertedPhotonColl");
   beamSpotColl = iConfig.getParameter<edm::InputTag>("BeamSpot");
+  caloTowerColl = iConfig.getParameter<edm::InputTag>("CaloTowerColl");
   trackColl = iConfig.getParameter<edm::InputTag>("TrackColl");
   trackColl2 = iConfig.getParameter<edm::InputTag>("TrackColl3");
   vertexColl = iConfig.getParameter<edm::InputTag>("VertexColl_std");
@@ -283,6 +288,10 @@ void GlobeElectrons::defineBranch(TTree* tree) {
   sprintf(a2, "el_%s_hcaliso03[el_%s_n]/F", nome, nome);
   tree->Branch(a1, &el_hcaliso03, a2);
 
+  sprintf(a1, "el_%s_hcalsolidiso03", nome);
+  sprintf(a2, "el_%s_hcalsolidiso03[el_%s_n]/F", nome, nome);
+  tree->Branch(a1, &el_hcalsolidiso03, a2);
+
   sprintf(a1, "el_%s_ecaliso03", nome);
   sprintf(a2, "el_%s_ecaliso03[el_%s_n]/F", nome, nome);
   tree->Branch(a1, &el_ecaliso03, a2);
@@ -294,6 +303,10 @@ void GlobeElectrons::defineBranch(TTree* tree) {
   sprintf(a1, "el_%s_hcaliso04", nome);
   sprintf(a2, "el_%s_hcaliso04[el_%s_n]/F", nome, nome);
   tree->Branch(a1, &el_hcaliso04, a2);
+
+  sprintf(a1, "el_%s_hcalsolidiso04", nome);
+  sprintf(a2, "el_%s_hcalsolidiso04[el_%s_n]/F", nome, nome);
+  tree->Branch(a1, &el_hcalsolidiso04, a2);
 
   sprintf(a1, "el_%s_ecaliso04", nome);
   sprintf(a2, "el_%s_ecaliso04[el_%s_n]/F", nome, nome);
@@ -421,6 +434,9 @@ bool GlobeElectrons::analyze(const edm::Event& iEvent, const edm::EventSetup& iS
   edm::Handle<reco::BeamSpot> bsHandle;
   iEvent.getByLabel(beamSpotColl, bsHandle);
   const reco::BeamSpot &thebs = *bsHandle.product();
+
+  edm::Handle<edm::SortedCollection<CaloTower> > ctH;
+  iEvent.getByLabel(caloTowerColl, ctH);
 
   edm::Handle<reco::TrackCollection> tkH;
   iEvent.getByLabel(trackColl, tkH);
@@ -789,6 +805,12 @@ bool GlobeElectrons::analyze(const edm::Event& iEvent, const edm::EventSetup& iS
     el_tkiso03[el_n] = egsf.dr03TkSumPt();
     el_ecaliso03[el_n] = egsf.dr03EcalRecHitSumEt();
     el_hcaliso03[el_n] = egsf.dr03HcalTowerSumEt();
+
+    EgammaTowerIsolation hcaliso03(0.3, 0., 0, -1, ctH.product());
+    EgammaTowerIsolation hcaliso04(0.4, 0., 0, -1, ctH.product());
+
+    el_hcalsolidiso03[el_n] = hcaliso03.getTowerEtSum(&(*(egsf.superCluster())) );
+    el_hcalsolidiso04[el_n] = hcaliso04.getTowerEtSum(&(*(egsf.superCluster())) );
 
     if (egsf.isEB()) {
       std::vector<reco::PFCandidate::ParticleType> temp;
