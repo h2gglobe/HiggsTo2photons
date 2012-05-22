992831aeef6e5dac74f1807fa437a712e3bb8a67
drberry <>
2012-05-22 22:03:43 +0000

    Fixed Collection Name

--- python/photonInvariantMassFilter_cfi.py
+++ python/photonInvariantMassFilter_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 photonInvariantMassFilter = cms.EDFilter("PhotonInvariantMassFilter",
-                                         photonCollection = cms.InputTag("goodPhotonsHighPtCut"),
+                                         photonCollection = cms.InputTag("goodPhotonsLowPtCut"),
                                          minMassCut = cms.double(45.),
                                          maxMassCut = cms.double(250.)
                                          )
