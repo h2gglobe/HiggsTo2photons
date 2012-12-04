c4ce44e6d5133ca208af42e71131b4aa2e926657
drberry <>
2012-12-04 19:56:57 +0000

    Fixed eos find statement

--- scripts/filecleaner.py
+++ scripts/filecleaner.py
@@ -161,7 +161,7 @@ if len(rootfiles)!=0:
 
 if options.recursive:
     if options.castor: subdirs = popen("nsfind "+dir+" -type d ").readlines()
-    if options.eos: subdirs = popen(eos+" find -d "+dir+" | grep /").readlines()
+    if options.eos: subdirs = popen(eos+" find -d "+dir+" | grep / | awk '{print $1}'").readlines()
     for i in range(len(subdirs)):
         subdir = subdirs[i].strip("\n")
         if (options.duplicate): removeduplicated(subdir)
