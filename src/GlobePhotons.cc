cd4804bf2d8449bca603b4120f553716234b37d8
sani <>
2013-05-10 13:13:43 +0000

    update

--- src/GlobePhotons.cc
+++ src/GlobePhotons.cc
@@ -170,6 +170,7 @@ void GlobePhotons::defineBranch(TTree* tree) {
   tree->Branch("pho_see",&pho_see,"pho_see[pho_n]/F");
   tree->Branch("pho_sieie",&pho_sieie,"pho_sieie[pho_n]/F");
   tree->Branch("pho_e1x5",&pho_e1x5,"pho_e1x5[pho_n]/F");
+  tree->Branch("pho_e1x3",&pho_e1x3,"pho_e1x3[pho_n]/F");
   tree->Branch("pho_e2x2",&pho_e2x2,"pho_e2x2[pho_n]/F");
   tree->Branch("pho_e3x3",&pho_e3x3,"pho_e3x3[pho_n]/F");
   tree->Branch("pho_e5x5",&pho_e5x5,"pho_e5x5[pho_n]/F");
@@ -743,6 +744,7 @@ bool GlobePhotons::analyze(const edm::Event& iEvent, const edm::EventSetup& iSet
     pho_sieie[pho_n] = localPho->sigmaIetaIeta();
     pho_e1x5[pho_n] = localPho->e1x5();
     pho_e2x2[pho_n] = lazyTool.e2x2(*seed_clu);
+    pho_e1x3[pho_n] = lazyTool.e1x3(*seed_clu);
     pho_e3x3[pho_n] = localPho->e3x3();
     pho_e5x5[pho_n] = localPho->e5x5();
     pho_emaxxtal[pho_n] = localPho->maxEnergyXtal();
