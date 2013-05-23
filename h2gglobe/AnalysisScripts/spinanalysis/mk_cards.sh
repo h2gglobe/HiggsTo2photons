039129fe84efa72dcce561c4ebe7d57b5f430315
amarini <>
2013-05-23 14:13:31 +0000

    This commit was manufactured by cvs2svn to create tag 'ttH_branch_20130523'.
    Sprout from ttH_branch_20130422 2013-05-23 14:01:33 UTC micheli 'first commit'
    Cherrypick from master 2013-05-23 14:13:30 UTC micheli ' first commit tth branch':
        h2gglobe/GeneralFunctions.cc
    Delete:
        h2gglobe/AnalysisScripts/check_hists.py
        h2gglobe/AnalysisScripts/spinanalysis/datacard_spinanalysis.txt
        h2gglobe/AnalysisScripts/spinanalysis/datacard_spinanalysis_justGrav.txt
        h2gglobe/AnalysisScripts/spinanalysis/datacard_spinanalysis_justSM.txt
        h2gglobe/AnalysisScripts/spinanalysis/datacard_spinanalysis_qqbar.txt
        h2gglobe/AnalysisScripts/spinanalysis/datafiles_spinanalysis.dat
        h2gglobe/AnalysisScripts/spinanalysis/merge_jobs.py
        h2gglobe/AnalysisScripts/spinanalysis/mk_cards.sh
        h2gglobe/AnalysisScripts/spinanalysis/mk_spin_card.py
        h2gglobe/AnalysisScripts/spinanalysis/mk_spin_plots.py
        h2gglobe/AnalysisScripts/spinanalysis/spin_plotvariables.dat
        h2gglobe/AnalysisScripts/spinanalysis/spin_treevariables.dat
        h2gglobe/AnalysisScripts/spinanalysis/spinanalysis.dat
        h2gglobe/AnalysisScripts/spinanalysis/sub_spin_jobs.py
        h2gglobe/Macros/FinalResults/example.dat
        h2gglobe/Macros/FullMvaToolkit/dat/moriond2013_postChecks.dat
        h2gglobe/Macros/FullMvaToolkit/interface/LocalRegression.h
        h2gglobe/Macros/FullMvaToolkit/interface/Optimizations.h
        h2gglobe/Macros/FullMvaToolkit/python/makeNormGraph.py
        h2gglobe/Macros/FullMvaToolkit/src/Optimizations.cc
        h2gglobe/SpinAnalysis/catsPlot.py
        h2gglobe/SpinAnalysis/checkCatsDIY.py
        h2gglobe/SpinAnalysis/checkRandCats.py
        h2gglobe/SpinAnalysis/subCDFCatsDIY.py
        h2gglobe/SpinAnalysis/subCatsCommon.py
        h2gglobe/SpinAnalysis/subCatsDIY.py
        h2gglobe/SpinAnalysis/subFixedCatsDIY.py
        h2gglobe/SpinAnalysis/subJSONCatsDIY.py
        h2gglobe/SpinAnalysis/subRandCatsDIY.py

--- h2gglobe/AnalysisScripts/spinanalysis/mk_cards.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-./mk_spin_card.py -n datacard_spinanalysis.txt
-./mk_spin_card.py -n datacard_spinanalysis_qqbar.txt -q
-./mk_spin_card.py -n datacard_spinanalysis_justSM.txt -s
-./mk_spin_card.py -n datacard_spinanalysis_justGrav.txt -g
-
